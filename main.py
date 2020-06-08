9446bb5716d70c12749f28aa89b37965d2edb9b3
SukruGokk <62439670+SukruGokk@users.noreply.github.com>
2020-06-08 16:25:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import bs4
+import requests
+
+euroURL = "http://tr.investing.com/currencies/eur-try"
+dolarURL = "http://tr.investing.com/currencies/usd-try"
+
+def get_euro():
+	# CREATES A SESSION
+	with requests.Session() as euroSes:
+
+		# PAGE CONTENT
+		cnt = euroSes.get(euroURL, headers={"User-Agent": "Mozilla"}).content
+		cnt = BS(cnt, "html5lib")
+
+		# EURO VALUE
+		euro = cnt.find('span', id="last_last").string
+
+		return euro
+
+def get_dolar():
+	# CREATES A SESSION
+	with requests.Session() as dolarSes:
+
+		# PAGE CONTENT
+		cnt = dolarSes.get(euroURL, headers={"User-Agent": "Mozilla"}).content
+		cnt = BS(cnt, "html5lib")
+
+		# DOLAR VALUE
+		dolar = cnt.find('span', id="last_last").string
+
+		return dolar
